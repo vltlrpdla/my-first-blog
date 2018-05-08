0047e9e3bfc1506050990c8a2e4648173920d009
vltlrpdla@hanmail.net <vltlrpdla@hanmail.net>
2018-05-08 23:58:52 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     #현재 시간 기준 과거에 작성했던 게시글 모두를 가져오고 게시일 기준으로 정렬해서 가져오는 구문
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     #posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})    
