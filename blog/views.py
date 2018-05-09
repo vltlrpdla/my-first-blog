15e2f9c09b3a457460cb9211c9a61ec13ea6e58c
vltlrpdla@hanmail.net <vltlrpdla@hanmail.net>
2018-05-09 23:56:22 +0900

    Added views to create/edit blog post inside the site.
    end of the tutorial, I will start advanced tutorial of this site

--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,46 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
+from django.shortcuts import redirect
 
+#리스트뷰
 def post_list(request):
     #현재 시간 기준 과거에 작성했던 게시글 모두를 가져오고 게시일 기준으로 정렬해서 가져오는 구문
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     #posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+#상세보기
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})    
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+#포스트 생성
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+#포스트 수정
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
